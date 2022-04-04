8c16f4aab61a58bbfbc2270219d13e2541285640
sbx_user1051 <sbx_user1051@169.254.136.85>
2022-04-04 19:53:31 +0000

    Mon Apr 04 19:53:31 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,4 +3,60 @@
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
-    raise NotImplementedError()
+    items = list(skus)
+    items_set = set(items)
+    stock = "ABCD"
+    prices = {"A": 50, "B": 30, "C": 20, "D": 15}
+    special_offer = "AB"
+
+    total = 0
+    for item in items_set:
+        if item not in stock:
+            return -1
+        
+        amount = items.count(item)
+        if item not in special_offer:
+            total += (amount * prices.get(item))
+        else:
+            if item == "A":
+                q, rest = divmod(amount, 3)
+                total += (q*130 + rest*prices.get(item))
+            elif item == "B":
+                q, rest = divmod(amount, 2)
+                total += (q*45 + rest*prices.get(item))
+
+    return total
+
+
+#print(checkout("AAAA"))
+#print(checkout("BBBB"))
+#print(checkout("C"))
+#print(checkout("D"))
+#print(checkout("ABCD"))
+        
+
+        
+
+
+"""
+Our price table and offers: 
++------+-------+----------------+
+| Item | Price | Special offers |
++------+-------+----------------+
+| A    | 50    | 3A for 130     |
+| B    | 30    | 2B for 45      |
+| C    | 20    |                |
+| D    | 15    |                |
++------+-------+----------------+
+
+
+Notes: 
+ - For any illegal input return -1
+
+In order to complete the round you need to implement the following method:
+     checkout(String) -> Integer
+
+Where:
+ - param[0] = a String containing the SKUs of all the products in the basket
+ - @return = an Integer representing the total checkout value of the items 
+ """
