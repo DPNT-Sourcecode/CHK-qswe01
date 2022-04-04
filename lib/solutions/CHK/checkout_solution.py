af7ef5d0b80e4645f8e62f3a4546baa3f15263ff
sbx_user1051 <sbx_user1051@169.254.136.85>
2022-04-04 20:08:32 +0000

    Mon Apr 04 20:08:32 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -7,9 +7,7 @@ def checkout(skus):
     items_set = set(items)
     stock = "ABCDE"
     prices = {"A": 50, "B": 30, "C": 20, "D": 15, "E": 40}
-    special_offer = "AB"
-
-    total = 0
+    special_offer = "ABE"
 
     basket = {}
     for item in items_set:
@@ -18,34 +16,32 @@ def checkout(skus):
 
         basket[item] = items.count(item)
 
-    return basket
-
-    #for item in items_set:
-    #    if item not in stock:
-    #        return -1
-    #    
-    #    amount = items.count(item)
-    #    if item not in special_offer:
-    #        total += (amount * prices.get(item))
-    #    else:
-    #        if item == "A":
-    #            q5A, rest5 = divmod(amount, 5)
-    #            q3A, rest3 = divmod(rest5, 3)
-    #            total += (q5A*200 + q3A*130 + rest3*prices.get(item))
-    #        elif item == "B":
-    #            q, rest = divmod(amount, 2)
-    #            total += (q*45 + rest*prices.get(item))
-    #        elif item == "E":
-#
+    total = 0
+    for item, amount in basket.items():
+        if item not in special_offer:
+            total += amount * prices.get(item)
+        else:
+            if item == "A":
+                q5A, rest5 = divmod(amount, 5)
+                q3A, rest3 = divmod(rest5, 3)
+                total += (q5A*200 + q3A*130 + rest3*prices.get(item))
+            elif item == "B":
+                q, rest = divmod(amount, 2)
+                total += (q*45 + rest*prices.get(item))
+            elif item == "E":
+                total += amount*prices.get(item)
+                if basket.get("B"):
+                    q, rest = divmod(amount, 2)
+                    total -= q*prices.get("B")
+    
     return total
-
 
 print(checkout("AAAA"))
 print(checkout("BBBB"))
 print(checkout("C"))
 print(checkout("D"))
 print(checkout("ABCD"))
-print(checkout("ABCDE"))
+print(checkout("ABCDEE"))
 print(checkout("AAAAAABCD"))
 
 
