d33b4cdc35250cdaf3228865d3c650ce241397cd
sbx_user1051 <sbx_user1051@169.254.109.197>
2022-04-05 06:28:03 +0000

    Tue Apr 05 06:28:03 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -5,13 +5,12 @@
 def checkout(skus):
     items = list(skus)
     items_set = set(items)
-    stock = "ABCDE"
-    prices = {"A": 50, "B": 30, "C": 20, "D": 15, "E": 40}
-    special_offer = "AB"
+    prices = {"A": 50, "B": 30, "C": 20, "D": 15, "E": 40, "F": 10}
+    special_offer = "ABF"
 
     basket = {}
     for item in items_set:
-        if item not in stock:
+        if item not in prices.keys():
             return -1
 
         basket[item] = items.count(item)
@@ -40,6 +39,11 @@ def checkout(skus):
 
                 qB, restB = divmod(newAmountB, 2)
                 total += qB*45 + restB*prices.get(item)
+            elif item == "F":
+                qF, restF = divmod(basket.get(item), 3)
+                newAmountB = basket.get(item) - qF
+
+                total += newAmountB * prices.get(item)
                 
     return total
 
@@ -55,6 +59,10 @@ def checkout(skus):
 #print(checkout("ABCDEABCDE"))   #280
 #print(checkout("EEB")) # 80
 #print(checkout("EEEB")) # 120
+print(checkout("FF"))
+print(checkout("FFF"))
+print(checkout("FFFFF"))
+print(checkout("FFFFFF"))
 
         
 
@@ -62,6 +70,11 @@ def checkout(skus):
 
 
 """
+A new item has arrived. Item F.
+Our marketing team wants to try rewording the offer to see if it affects consumption
+Instead of multi-pricing this item they want to say "buy 2Fs and get another F free"
+The offer requires you to have 3 Fs in the basket.
+
 Our price table and offers: 
 +------+-------+------------------------+
 | Item | Price | Special offers         |
@@ -71,18 +84,6 @@ Our price table and offers:
 | C    | 20    |                        |
 | D    | 15    |                        |
 | E    | 40    | 2E get one B free      |
+| F    | 10    | 2F get one F free      |
 +------+-------+------------------------+
-
-
-Notes: 
- - The policy of the supermarket is to always favor the customer when applying special offers.
- - All the offers are well balanced so that they can be safely combined.
- - For any illegal input return -1
-
-In order to complete the round you need to implement the following method:
-     checkout(String) -> Integer
-
-Where:
- - param[0] = a String containing the SKUs of all the products in the basket
- - @return = an Integer representing the total checkout value of the items 
  """
