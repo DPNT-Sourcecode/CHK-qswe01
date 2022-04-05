96760dbb0b1519502c5e5f3bdddf65be4e62e938
sbx_user1051 <sbx_user1051@169.254.204.13>
2022-04-05 07:32:58 +0000

    Tue Apr 05 07:32:58 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -5,7 +5,12 @@
 def checkout(skus):
     items = list(skus)
     items_set = set(items)
-    prices = {"A": 50, "B": 30, "C": 20, "D": 15, "E": 40, "F": 10}
+    prices = {"A": 50, 
+    "B": 30, 
+    "C": 20, 
+    "D": 15, 
+    "E": 40, 
+    "F": 10}
     special_offer = "ABF"
 
     basket = {}
@@ -70,12 +75,6 @@ print(checkout("FFFFFF"))
 
 
 """
-A new item has arrived. Item F.
-Our marketing team wants to try rewording the offer to see if it affects consumption
-Instead of multi-pricing this item they want to say "buy 2Fs and get another F free"
-The offer requires you to have 3 Fs in the basket.
-
-Our price table and offers: 
 +------+-------+------------------------+
 | Item | Price | Special offers         |
 +------+-------+------------------------+
@@ -85,5 +84,25 @@ Our price table and offers:
 | D    | 15    |                        |
 | E    | 40    | 2E get one B free      |
 | F    | 10    | 2F get one F free      |
+| G    | 20    |                        |
+| H    | 10    | 5H for 45, 10H for 80  |
+| I    | 35    |                        |
+| J    | 60    |                        |
+| K    | 80    | 2K for 150             |
+| L    | 90    |                        |
+| M    | 15    |                        |
+| N    | 40    | 3N get one M free      |
+| O    | 10    |                        |
+| P    | 50    | 5P for 200             |
+| Q    | 30    | 3Q for 80              |
+| R    | 50    | 3R get one Q free      |
+| S    | 30    |                        |
+| T    | 20    |                        |
+| U    | 40    | 3U get one U free      |
+| V    | 50    | 2V for 90, 3V for 130  |
+| W    | 20    |                        |
+| X    | 90    |                        |
+| Y    | 10    |                        |
+| Z    | 50    |                        |
 +------+-------+------------------------+
  """
